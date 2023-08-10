428370d1a2bde26f4445747441c2bced0b0818e0
ELouaradi <amineneymar10@gmail.com>
2023-08-10 16:37:57 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Emtry point
+*
+*Return Always 0 (Success)
+*/
+int main(void)
+{
+    printf("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
