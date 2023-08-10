e9eae8e6b0182dd097169a8642e96362ea2ccd15
ELouaradi <amineneymar10@gmail.com>
2023-08-10 16:55:53 -0700

    putsss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 *main - Emtry point
 *
-*Return Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
